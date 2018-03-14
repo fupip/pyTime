521fda21e103428757bdf557383e2826cc644791
fupip <fupip@sina.com>
2018-03-14 17:17:22 +0800

    format code

--- RangeTime.py
+++ RangeTime.py
@@ -1,5 +1,6 @@
-
+# -*- coding: utf-8 -*-
 def Enum(**enums):
     return type('Enum', (), enums)
+
 RangeTime = Enum(day_break=3, early_morning=8, morning=10,
                  noon=12, afternoon=15, night=18, latenight=20, midnight=23)
